967983497070336e50717a09469ff42957830e5b
unknown <ficep@.(none)>
2014-12-08 14:58:23 +0100

    techn

--- src/Ficep/PlanningBundle/Controller/TechnicianController.php
+++ src/Ficep/PlanningBundle/Controller/TechnicianController.php
@@ -50,4 +50,52 @@ class TechnicianController extends Controller
 		return $this->render('FicepPlanningBundle:Technician:list.html.twig', array('technicians' => $technicians ));
 	}
 	
+	public function deleteAction($id)
+	{
+		$repository = $this->getDoctrine()->getManager()->getRepository('FicepPlanningBundle:Technician');
+		$em = $this->getDoctrine()->getManager();
+		$technician = $repository->find($id);
+		if ( !$technician )
+		{
+			throw $this->createNotFoundException('Ce technicien n\'existe pas');
+		}
+		
+		$em->remove($technician);
+		$em->flush();
+		$session = $this->container->get('session');
+		$session->getFlashBag()->add('notice', 'Technicien bien supprimé.');
+		return $this->redirect($this->generateUrl('ficep_planning_listTechnician', array('id' => 'all')));
+	}
+	
+	public function editAction($id, Request $request)
+	{
+		$repository = $this->getDoctrine()->getManager()->getRepository('FicepPlanningBundle:Technician');
+		$technician = $repository->find($id);
+		
+		
+		if (!$technician)
+		{
+			throw $this->createNotFoundException('Ce technicien n\'existe pas');
+		}
+		
+		$form = $this->get('form.factory')->create(new TechnicianType, $technician);
+
+		$form->handleRequest($request);
+		
+		if ($form->isValid())
+		{
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($technician);
+			$em->flush();
+			
+			//message pour confirmer que l'opération c est bien passé puis redirection vers le forulaire pour crééer un nouveau technicien
+			
+			$session = $this->container->get('session');
+			$session->getFlashBag()->add('notice', 'Technicien bien modifié.');
+			return $this->redirect($this->generateUrl('ficep_planning_listTechnician', array('id' => 'all')));
+		}
+		
+		 return $this->render('FicepPlanningBundle:Technician:add.html.twig', array('form' => $form->createView()));
+	}
 }
+
